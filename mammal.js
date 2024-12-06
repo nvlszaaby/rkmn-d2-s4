d917cda18755db014ddc7e4c03ef38508e35b99f
Gebby Novalisza <gebbynovalisza@student.telkomuniversity.ac.id>
2024-12-06 16:53:42 +0700

    fifth.js

---
+++ mammal.js
@@ -0,0 +1,18 @@
+class Animal{
+    #age
+    constructor(name,food, energy, canReproduce,age){
+        this.name = name
+        this.food = food
+        this.energy = energy
+        this.canReproduce = canReproduce
+        this.#age = age
+    }
+    get #Age(){
+        return this.#age
+    }
+    isAdult(){ //breaking encapsulation
+        return this.#age > 18
+        }
+    }
+
+    export default Animal;
